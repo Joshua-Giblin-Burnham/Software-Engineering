c20500fc7fdc01b7d7904bb82dbceed04aca7019
Charlie Hamilton <chamilton0024@gmail.com>
2023-10-19 13:51:17 +0100

    Added protocol testing

--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -10,7 +10,14 @@ class ModelTest(unittest.TestCase):
         """
         Tests Model creation.
         """
-        model = pk.Model('BLANCK')
+        model = pk.Model('BLANK')
 
         self.assertEqual(model.Q_p1, 2)
 
+    def test_add_protocol(self):
+
+        model = pk.Model('BLANK')
+        model.add_protocols(dose=2, name='JEFF')
+        self.assertEqual(model.protocol[0].name, 'JEFF')
+
+    
