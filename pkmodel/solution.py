41ef181dc9849422ed3fa78366c2b995149a1fd2
Christopher Chung <chris.chung@hotmail.co.uk>
2023-10-20 11:45:02 +0100

    Fixed Issue 36 - added individual model plotting method to solution class.

--- pkmodel/solution.py
+++ pkmodel/solution.py
@@ -12,13 +12,13 @@ class Solution:
 	----------
 	:param models: Model list holding solved models 
 	:type models: class"""
+
 	# Initialise class ensuring models variable is 1d list holding models
 	def __init__(self, models):
 		if isinstance(models, list) == True:
 			self.models = models 
 		else: 
 			self.models = [models]
-
 
 	def add_model(self, model):
 		""" Function to add model to models list.
@@ -36,8 +36,8 @@ class Solution:
 		else: 
 			self.models.append(model)
 	
-	def plot(self):
-		"""Function to plot results from model list"""
+	def plot_all(self):
+		"""Function to plot all models' results in one graph"""
 		# Initialise subplot and set line styles
 		fig, ax = plt.subplots(1,1)
 		linestyle = ['-.','--']
@@ -57,7 +57,7 @@ class Solution:
 			if model.protocol[-1].name == 'subcutaneous':
 				ax.plot(solution.t, solution.y[-1, :], ':', color = plt.gca().lines[-1].get_color(), label=model.name+' - q.dosing')
 
-			# Loop through and plot perpheral compartment solutions
+			# Loop through and plot peripheral compartment solutions
 			for n in range(N):
 				ax.plot(solution.t, solution.y[n+1, :], ls = np.roll(linestyle, n)[0], 
 						color = plt.gca().lines[-1].get_color(), label=model.name+" - q.peripheral_"+str(n+1))
@@ -68,4 +68,41 @@ class Solution:
 		ax.set_xlabel('Time (h)')
 		plt.legend()
 		plt.show()
+
+	def plot_indiv(self):
+		"""Function to plot individual model results"""
+
+		# Loop for each model in class list
+		for model in self.models:
+
+			# Initialise subplot and set line styles
+			fig, ax = plt.subplots(1,1)
+			linestyle = ['-.','--']
+
+			# Set number of peripheral compartment in model  
+			N = len(np.array(model.compartments['peripheral']))
+
+			# Call model solutions
+			solution = model.solution
+
+			# Plot central compartment solution corresponding to first solution
+			ax.plot(solution.t, solution.y[0, :], '-', label = 'Central')
+
+			# If subcutaneous/ has dosing compartment, plot dose compartment corresponding to last solution
+			if model.protocol[-1].name == 'subcutaneous':
+				ax.plot(solution.t, solution.y[-1, :], ':', color = plt.gca().lines[-1].get_color(), label='Dosing')
+
+			# Loop through and plot peripheral compartment solutions
+			for n in range(N):
+				ax.plot(solution.t, solution.y[n+1, :], ls = np.roll(linestyle, n)[0], 
+						color = plt.gca().lines[-1].get_color(), label = "Peripheral_"+str(n+1))
+	  
+		  	# Set labels and legend
+			plt.title(model.name + ' - ' + model.protocol[-1].name)
+			ax.set_ylabel('Drug mass (ng)')
+			ax.set_xlabel('Time (h)')
+			plt.legend()
+			plt.show()
+
+			print(model.protocol[-1].name)
 
