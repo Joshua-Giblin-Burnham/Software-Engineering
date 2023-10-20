3d1fa73fc75a0e4d8865eb2b6dd7db601c929078
Joshua-Giblin-Burnham <j.giblin-burnham@hotmail.com>
2023-10-20 13:51:49 +0100

    Added exception for ill defined compartment

--- pkmodel/compartment.py
+++ pkmodel/compartment.py
@@ -9,7 +9,7 @@ class Compartment:
     ----------
     :param name: Name/Type of compartment - must be either central, dose, or peripheral_X.
     :type name: string
-    :param volume: Volume of compartment, mush be positive.
+    :param volume: The volume of compartment, mush be positive and if not set (as not required for dose) it is set to None, which errors if central or peripheral compartment
     :type volume: float
     :param k_rate: Transition rate out of a given compartment. Depending on compartment type is CL, k_a, or Q_Pi.
     :type k_rate: float
@@ -19,6 +19,10 @@ class Compartment:
 
     def __init__(self, name, volume=None, k_rate=None, q0 = 0 ):
         # Ensure that volume and q0 values are feasible
+        if volume == None and name!='dose':
+            raise ValueError("Volume must be set for this compartment")
+        if k_rate == None:
+            raise ValueError("Transition rate must be set for this compartment")        
         if volume!=None and volume < 0:
             raise ValueError("Volume must be greater than zero")
         elif q0 < 0:
