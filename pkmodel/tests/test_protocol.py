89ca5887cf6b700679c3fe5b16deebc4827d8bc2
Charlie Hamilton <chamilton0024@gmail.com>
2023-10-20 14:39:22 +0100

    Added protocol test, fixed raise exceptions

--- pkmodel/tests/test_protocol.py
+++ pkmodel/tests/test_protocol.py
@@ -2,6 +2,8 @@ import unittest
 import pkmodel as pk
 import numpy as np
 
+def dose(t, X):
+    return X
 
 class ProtocolTest(unittest.TestCase):
     """
@@ -11,6 +13,15 @@ class ProtocolTest(unittest.TestCase):
         """
         Tests Protocol creation.
         """
-        protocol = pk.Protocol(name ='subcutaneous', dose=2, compartments=[])
+        protocol = pk.Protocol(name ='subcutaneous', dose=dose, compartments=[])
         self.assertEqual(protocol.name, 'subcutaneous')
 
+    def test_wrong_name(self):
+        """
+        Tests putting the wrong name.
+        """
+
+        with self.assertRaises(ValueError) as cm:
+            protocol = pk.Protocol(name ='Jeff', dose=dose, compartments=[])
+
+        self.assertEqual('Not accepted protocol label, must be intravenous or subcutaneous', str(cm.exception))
